70c735e86d14f326ba932e8bb5f481aab7686bf7
Cameron Murphy <camurphy@gmail.com>
2015-03-20 10:44:31 +1100

    Created solution

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Camurphy.InternetConnectionMonitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
